2b4997ee5aa6874d0ee233654f5f7668374f8ce7
장수제/201311240 <sujaes@naver.com>
2017-06-05 14:49:52 +0900

    Dao_v2

---
+++ src/com/sd/headfirst/dao/v2/PersonDAO.java
@@ -0,0 +1,10 @@
+package com.sd.headfirst.dao.v2;
+import java.util.List;
+
+public interface PersonDAO {
+    public void insert(PersonVO person);
+    public List<PersonVO> findAll();
+    public PersonVO findById(int id);
+    public void update(PersonVO person);
+    public void delete(int id);
+}

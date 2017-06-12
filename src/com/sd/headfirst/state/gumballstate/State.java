b47c02d4cfea1aa2fd5a8785c68646ada9e83aca
장수제/201311240 <sujaes@naver.com>
2017-06-12 14:43:49 +0900

    state

---
+++ src/com/sd/headfirst/state/gumballstate/State.java
@@ -0,0 +1,8 @@
+package com.sd.headfirst.state.gumballstate;
+
+public interface State {
+ public void insertQuarter();
+ public void ejectQuarter();
+ public void turnCrank();
+ public void dispense();
+}
